6a87bb19abe8f3d756efebe00cb2252437693808
Vlad <126715469+Vladpru@users.noreply.github.com>
2025-03-12 17:58:28 +0200

    Update settings.py

--- sessionauth/sessionauth/settings.py
+++ sessionauth/sessionauth/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'NewAuth',
         'USER': 'root',
-        'PASSWORD': 'ihorr911',
+        'PASSWORD': '#',
         'HOST': 'localhost',
         'PORT': '3306',
     }
